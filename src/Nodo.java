7e4a79c251b71cd3837d3134260249ed0daa13e5
Mv05007 <mauvalenciam891@gmail.com>
2026-06-17 00:41:38 -0500

    Create Nodo class for binary tree nodes

---
+++ src/Nodo.java
@@ -0,0 +1,11 @@
+public class Nodo {
+    public int valor;
+    public Nodo izquierdo;
+    public Nodo derecho;
+
+    public Nodo(int valor) {
+        this.valor = valor;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+}
